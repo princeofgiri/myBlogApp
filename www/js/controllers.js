c0b1f32336a9aed36f4e2e34e97d6c6079a1fa19
Ahmad Haris <ahmadharis1982@gmail.com>
2016-04-06 20:55:00 +0700

    * fix html character escaping

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,13 +1,17 @@
 angular.module('starter')
-.controller('MenuCtrl', function($http, $scope){
+.controller('MenuCtrl', function($http, $scope, $sce){
 
 	$scope.categories = [];
+
 	$http.get("http://newsportal.lightsandshapes.in/api/get_category_index/")
 	.then(function(returnedData){
 
 		$scope.categories = returnedData.data.categories;
+		$scope.categories.forEach(function(element, index, array){
+			element.title = $sce.trustAsHtml(element.title);
+		})
 		console.log(returnedData);
-
+		
 	}, function(err){
 		console.log(err);
 
